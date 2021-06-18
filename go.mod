82c5073bc3803c9e12b40167b6253b300f0a3edb
ayush5588 <ayushsinha126@gmail.com>
2021-06-18 14:16:47 +0000

    Added metrics collection for pod_status

--- go.mod
+++ go.mod
@@ -2,7 +2,12 @@ go 1.13
 
 require (
 	github.com/fsnotify/fsnotify v1.4.9
+	github.com/hashicorp/consul/api v1.8.1 // indirect
 	github.com/hashicorp/golang-lru v0.5.1
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/prometheus/prometheus v2.5.0+incompatible // indirect
+	github.com/samuel/go-zookeeper v0.0.0-20201211165307-7117e9ea2414 // indirect
+	gopkg.in/fsnotify/fsnotify.v1 v1.4.7 // indirect
 	k8s.io/api v0.19.6
 	k8s.io/apimachinery v0.19.6
 	k8s.io/client-go v0.19.6
--- pkg/prometheusMetrics/main.go
+++ pkg/prometheusMetrics/main.go
@@ -7,17 +7,53 @@ import (
    "encoding/json"
    "log"
    "net/http"
-   "fmt"
+//   "fmt"
 
    metricsStruct "github.com/ayush5588/ClusterAutoscaler/pkg/metrics/metricsStruct"
 )
 
 
+type TempPodStatusStruct struct {
+    PodName string
+    PodPhase string
+    PhaseStatus string
+}
+
+
+func PodStatusPhase (promServerIP string) ([]TempPodStatusStruct, error) {
+    query := "kube_pod_status"
+    resp, err := http.Get(promServerIP+query)
+    if err != nil {
+        return nil, err
+    }
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return nil, err
+    }
+    var pod metricsStruct.PodStatusPhaseStruct
+    err = json.Unmarshal(body,&pod)
+    if err != nil {
+        return nil, err
+    }
+    var PodStatusPhaseArr []TempPodStatusStruct
+    for _, p := range pod.Data.Result {
+        var tempPod TempPodStatusStruct
+        tempPod.PodName = p.Metric.Pod
+        tempPod.PodPhase = p.Metric.Phase
+        tempPod.PhaseStatus = p.Value[1]
+        PodStatusPhaseArr = append(PodStatusPhaseArr,tempPod)
+    }
+    return PodStatusPhaseArr, nil
+}
+
+
+/*
 func main() {
-   /* 10.103.151.144 is the ClusterIP address of the prometheus-server service */
-   resp, err := http.Get("http://10.103.151.144:80/api/v1/query?query=kube_node_info")
-   /* metrics.NodeInfoStruct is a structure for kube_node_info which is defined in the metricsStruct.go of metrics package */
-   var node metricsStruct.NodeInfoStruct
+   //10.103.151.144 is the ClusterIP address of the prometheus-server service 
+   resp, err := http.Get("http://10.101.202.25:80/api/v1/query?query=kube_node_status_condition")
+   // metrics.NodeInfoStruct is a structure for kube_node_info which is defined in the metricsStruct.go of metrics package
+   //var node metricsStruct.NodeInfoStruct
+   var node metricsStruct.NodeStatusStruct
    if err != nil {
       log.Fatalln(err)
    }
@@ -26,14 +62,14 @@ func main() {
    if err != nil {
       log.Fatalln(err)
    }
-
    err = json.Unmarshal(body,&node)
+   //fmt.Println(pod)
    if err != nil {
         log.Fatalln(err)
    }
    for _, d := range node.Data.Result {
-        fmt.Println(d.Metric.Node)
-        fmt.Println(d.Metric.InternalIP)
+       fmt.Printf("Node: %s\nCondition: %s\nStatus: %s\nValue: %s",d.Metric.Node,d.Metric.Condition,d.Metric.Status,d.Value[1])
+       fmt.Println("\n")
    }
 
-}
+}*/
