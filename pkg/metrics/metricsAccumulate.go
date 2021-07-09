550f6918493edc3bd1148039bd01c6404fb6d088
ayush5588 <ayushsinha126@gmail.com>
2021-07-09 12:59:42 +0000

    Pressure Status and Unscheduled Pods output in table format

--- pkg/metrics/metricsAccumulate.go
+++ pkg/metrics/metricsAccumulate.go
@@ -55,22 +55,48 @@ func Start() {
         log.Fatal(err)
     }
     
+    PIDStatusMap := make(map[string]bool)
+    MEMStatusMap := make(map[string]bool)
+    DISKStatusMap := make(map[string]bool)
+
     for _, n := range tempNodeStatusArr {
         if n.ConditionStatus == "true" && n.ConditionStatusValue == "1" {
             if n.Condition == "PIDPressure" {
                 // UPSCALE
-                fmt.Printf("UPSCALE due to node -> %s\n", n.NodeName)
+                //fmt.Printf("UPSCALE due to node -> %s\n", n.NodeName)
+                PIDStatusMap[n.NodeName] = true
             }else if n.Condition == "MemoryPressure" {
                 // UPSCALE
-                fmt.Printf("UPSCALE due to node -> %s\n", n.NodeName)
+                //fmt.Printf("UPSCALE due to node -> %s\n", n.NodeName)
+                MEMStatusMap[n.NodeName] = true
             }else if n.Condition == "DiskPressure" {
                 // UPSCALE
-                fmt.Printf("UPSCALE due to node -> %s\n", n.NodeName)
+                //fmt.Printf("UPSCALE due to node -> %s\n", n.NodeName)
+                DISKStatusMap[n.NodeName] = true
             }else{
-                fmt.Printf("No PIDPressure OR MemoryPressure OR DiskPressure in node -> %s\n", n.NodeName)
+                //fmt.Printf("No PIDPressure OR MemoryPressure OR DiskPressure in node -> %s\n", n.NodeName)
+                PIDStatusMap[n.NodeName] = false
+                MEMStatusMap[n.NodeName] = false
+                DISKStatusMap[n.NodeName] = false
             }
         }
             //fmt.Println("NO Issues with PIDPressure OR MemoryPressure OR DiskPressure!!\n\n")
+    }
+
+    fmt.Println("NODE\t\tPIDPressure\tMEMPressure\tDISKPressure\tStatus\n")
+    for nodeName, _ := range PIDStatusMap {
+        if nodeName == "master" {
+            fmt.Printf("%s\t\t", nodeName)
+        }else {
+            fmt.Printf("%s\t", nodeName)
+        }
+
+        fmt.Printf("%v\t\t%v\t\t%v\t\t", PIDStatusMap[nodeName], MEMStatusMap[nodeName], DISKStatusMap[nodeName])
+        if PIDStatusMap[nodeName] || MEMStatusMap[nodeName] || DISKStatusMap[nodeName] {
+            fmt.Println("UPSCALE")
+        }else {
+            fmt.Println("NEUTRAL")
+        }
     }
 
     fmt.Println("\n\n")
@@ -81,21 +107,33 @@ func Start() {
         log.Fatal(err)
     }
 
+    var  UnscheduledPodsArr []string
     var cntUnscheduledPods int = 0
     for _, p := range tempPodsNotScheduledArr {
         if p.PodName != "" {
             cntUnscheduledPods += 1
-            fmt.Printf("\nUnscheduled Pod Name: %s", p.PodName)
+            UnscheduledPodsArr = append(UnscheduledPodsArr, p.PodName)
+           //fmt.Printf("\nUnscheduled Pod Name: %s", p.PodName)
             //break
         }
     }
 
     if cntUnscheduledPods >= 1 {
         // UPSCALE
-        fmt.Printf("\n\n%d Unscheduled Pods in the Cluster -> UPSCALE\n\n", cntUnscheduledPods)
+        /*
+        if cntUnscheduledPods == 1 {
+            fmt.Printf("\n\n%d Unscheduled Pods in the Cluster -> UPSCALE\n\n", cntUnscheduledPods)
+        }*/
+        fmt.Println("S.No\tUnscheduled POD\n")
+        for idx, podName := range UnscheduledPodsArr {
+            fmt.Printf("%d\t%s\n", idx+1, podName)
+        }
+        fmt.Println("\n\nSTATUS: UPSCALE\n\n")
     }else {
-        fmt.Println("No unscheduled pods\n\n")
+        fmt.Println("No unscheduled pods\n")
+        fmt.Println("\nSTATUS: NEUTRAL \n\n")
     }
+
     
     fmt.Println("\n")
 
@@ -155,18 +193,16 @@ func Start() {
         var upscale bool = false
         var downscale bool = false
         
-        //var memNodeUsage float64
-        //var cpuNodeUsage float64
 
         // Memory Utilization
         memAllocatable, exist :=  mp[n.NodeName+"-"+"memory"]
         if exist {
             memUsage := float64(n.NodeMemUsage * 1024)  // Converting to bytes (from Kibibyte)
-            //memNodeUsage = memUsage
 
             memoryUtilization := ( memUsage / memAllocatable) * 100
             //fmt.Printf("Memory Utilization = %0.2f\t", memoryUtilization)
             fmt.Printf("%0.2f\t", memoryUtilization)
+
             if memoryUtilization >= 50.00 {
                 upscale = true
             }else if memoryUtilization <= 20.00 {
@@ -187,6 +223,7 @@ func Start() {
             cpuUtilization := (cpuUsageCores / cpuAllocatableCores) * 100
             //fmt.Printf("CPU utilization : %0.2f\t\n", cpuUtilization)
             fmt.Printf("\t%0.2f\n", cpuUtilization)
+
             if cpuUtilization >= 50.00 {
                 upscale = true
             }else if cpuUtilization <= 20.00 {
@@ -196,10 +233,8 @@ func Start() {
                 //fmt.Printf("CPU Utilization is NEUTRAL: %0.2f\n\n", cpuUtilization)
             }
         }
-        //fmt.Println("\n\n")
+        
 
-        //nodeAction := make(map[string]string)
-        //nodeRemarks := make(map[string]string)
 
         if upscale {
             //fmt.Println("\nUPSCALE\n\n")
@@ -213,12 +248,12 @@ func Start() {
                     }
 
                     if destinationNode != "" {
-                        nodeAction[n.NodeName] = "Downscale"
+                        nodeAction[n.NodeName] = "DOWNSCALE"
                         nodeRemarks[n.NodeName] = "Can move pods to " + destinationNode
                         //fmt.Println("\nDOWNSCALE")
                         //fmt.Printf("Can move pods to node: %s\n\n\n", destinationNode)
                     } else {
-                        nodeAction[n.NodeName] = "Cannot Downscale"
+                        nodeAction[n.NodeName] = "CANNOT DOWNSCALE"
                         nodeRemarks[n.NodeName] = "Cannot downscale as no Nodes fulfill the resource requirement"
                         //fmt.Println("\nUnder utilized node but cannot DOWNSCALE as pods cannot be moved to other nodes\n\n")
                     }
@@ -228,8 +263,8 @@ func Start() {
                     //fmt.Println("\nUnder utilized node but cannot DOWNSCALE as pods of MASTER node cannot be moved to other nodes\n\n")
                 }
             }else {
-                nodeAction[n.NodeName] = "Neutral"
-                nodeRemarks[n.NodeName] = "No action needed"
+                nodeAction[n.NodeName] = "NEUTRAL"
+                nodeRemarks[n.NodeName] = "\tNo action needed"
                 //fmt.Println("\nNEUTRAL\n\n")
             }
         }
