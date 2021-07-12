5157f657565e1643ee85f572ac7c4aebfe0a4cb3
ayush5588 <ayushsinha126@gmail.com>
2021-07-12 12:19:15 +0000

    Commited final clean code

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
-/* UNDER DEVELOPMENT FILE*/
-
 package main
 
 import (
+    "fmt"
+    "time"
     ClusterAutoscaler "github.com/ayush5588/ClusterAutoscaler/pkg/metrics"
+
 )
-
-
 func main() {
-    ClusterAutoscaler.Start()
+
+    for i:=0; i<10; i++ {
+        fmt.Printf("-------------------CHECK %d-----------------\n\n",i+1)
+        ClusterAutoscaler.Start()
+        time.Sleep(20*time.Second)
+    }
 }
